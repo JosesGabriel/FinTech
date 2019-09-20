41c862e997f2792cc63a3ade7cf61908cde41e4c
nick <nickgayo12@gmail.com>
2019-09-20 18:50:29 +0800

    Card left sidebar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
         <v-card class="mb-3" light outlined>
-        <v-card-text class="pt-0">
+        <v-card-text class="py-0">
         <v-list>
-            <v-list-item class="pa-0">
-                <v-list-item-avatar class="mr-2">
+            <v-list-item class="px-0">
+                <v-list-item-avatar class="mr-3">
                     <v-img src="test.jpg"></v-img>
                 </v-list-item-avatar>
 
@@ -66,16 +66,14 @@
                     <v-icon color="#e01d61">mdi-alpha-v-circle-outline</v-icon>
                     </v-list-item-icon>
 
-                <router-link to="/vyndue" class="navbar__links">
-                    <v-list-item>
-                        <v-list-item-icon><v-icon color="#e01d61">mdi-alpha-v-circle-outline</v-icon></v-list-item-icon>
-                        <v-list-item-content><v-list-item-title>Vyndue</v-list-item-title></v-list-item-content>
-                    </v-list-item>
-                </router-link>
+                    <v-list-item-content>
+                    <v-list-item-title>Vyndue</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
             </v-list-item-group>
 
             <v-list-item-group>
-                <v-list-item-icon class="mx-0 my-2">
+                <v-list-item-icon>
                     <v-list-item-title class="caption">Power Tools</v-list-item-title>
                 </v-list-item-icon>
                 <v-list-item class="mb-0">
@@ -125,9 +123,5 @@
 }
 .navbar__card {
     position: fixed; 
-}
-.navbar__links {
-    color: white;
-    text-decoration: none;
 }
 </style>
---
+++ pages/social.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-container class="socialWall__container" :class="{'pa-0': $vuetify.breakpoint.xsOnly}">
+        <v-row class="mb-6" no-gutters>
+            <v-col class="px-3 hidden-md-and-down" cols="3" sm="3" md="3">
+              <Navbar/>
+            </v-col>
+            <Newsfeed/>
+            <v-col class="px-3 hidden-md-and-down" cols="3" sm="3" md="3">
+                <Trendingstocks/>
+                <Whotomingle/>
+                <Footersidebar/>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<style scoped>
+    .socialWall__container {
+        max-width: 1080px;
+        margin-top: 40px;
+    }
+</style>
+<script>
+import Navbar from '~/components/Navbar'
+import Newsfeed from '~/components/Newsfeed'
+import Trendingstocks from '~/components/Trendingstocks.vue'
+import Whotomingle from '~/components/Whotomingle.vue'
+import Footersidebar from '~/components/Footersidebar.vue'
+
+export default {
+  layout: 'social',
+  components: {
+      Navbar,
+      Newsfeed,
+      Trendingstocks,
+      Whotomingle,
+      Footersidebar
+  },
+  data() {
+      return {
+          isOpen: true
+      }
+  },
+  methods: {
+    toggle: function(){
+        this.isOpen = !this.isOpen
+    }
+  },
+  head () {
+    return {
+      title: 'Arbitrage',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: 'description', name: 'description', content: 'My custom description' }
+      ]
+    }
+  }
+}
+</script>
