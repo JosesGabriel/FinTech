d778d92e5301b3035d696c786609ee86b2994501
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-09-26 13:14:38 +0800

    Post Image ready + navbar margin adjustment

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -18,40 +18,40 @@
                 <v-list-item-group>
                     <router-link to="/" class="navbar__links">
                         <v-list-item>
-                            <v-list-item-icon><v-icon color="#dc8607">mdi-vector-square</v-icon></v-list-item-icon>
+                            <v-list-item-icon class="mr-4"><v-icon color="#dc8607">mdi-vector-square</v-icon></v-list-item-icon>
                             <v-list-item-content><v-list-item-title>Social Wall</v-list-item-title></v-list-item-content>
                         </v-list-item>
                     </router-link>
                     <router-link to="/chart" class="navbar__links">
                         <v-list-item>
-                            <v-list-item-icon><v-icon color="#bfb538">mdi-chart-bar</v-icon></v-list-item-icon>
+                            <v-list-item-icon class="mr-4"><v-icon color="#bfb538">mdi-chart-bar</v-icon></v-list-item-icon>
                             <v-list-item-content><v-list-item-title>Interactive Chart</v-list-item-title></v-list-item-content>
                         </v-list-item>
                     </router-link>
 
                     <router-link to="/journal" class="navbar__links">
                         <v-list-item>
-                            <v-list-item-icon><v-icon color="#367e45">mdi-library-books</v-icon></v-list-item-icon>
+                            <v-list-item-icon class="mr-4"><v-icon color="#367e45">mdi-library-books</v-icon></v-list-item-icon>
                             <v-list-item-content><v-list-item-title>Trading Journal</v-list-item-title></v-list-item-content>
                         </v-list-item>
                     </router-link>
 
                     <router-link to="/watchlist" class="navbar__links">
                         <v-list-item>
-                            <v-list-item-icon><v-icon color="#1a71b8">mdi-eye-plus-outline</v-icon></v-list-item-icon>
+                            <v-list-item-icon class="mr-4"><v-icon color="#1a71b8">mdi-eye-plus-outline</v-icon></v-list-item-icon>
                             <v-list-item-content><v-list-item-title>Watchers & Alerts</v-list-item-title></v-list-item-content>
                         </v-list-item>
                     </router-link>
                     <router-link to="/game" class="navbar__links">
                         <v-list-item>
-                            <v-list-item-icon><v-icon color="#9126a7">mdi-gamepad</v-icon></v-list-item-icon>
+                            <v-list-item-icon class="mr-4"><v-icon color="#9126a7">mdi-gamepad</v-icon></v-list-item-icon>
                             <v-list-item-content><v-list-item-title>Games</v-list-item-title></v-list-item-content>
                         </v-list-item>
                     </router-link>
 
                     <router-link to="/vyndue" class="navbar__links">
                         <v-list-item>
-                            <v-list-item-icon><v-icon color="#e01d61">mdi-alpha-v-circle-outline</v-icon></v-list-item-icon>
+                            <v-list-item-icon class="mr-4"><v-icon color="#e01d61">mdi-alpha-v-circle-outline</v-icon></v-list-item-icon>
                             <v-list-item-content><v-list-item-title>Vyndue</v-list-item-title></v-list-item-content>
                         </v-list-item>
                     </router-link>
@@ -62,7 +62,7 @@
                         <v-list-item-title class="caption">Power Tools</v-list-item-title>
                     </v-list-item-icon>
                     <v-list-item>
-                        <v-list-item-icon>
+                        <v-list-item-icon class="mr-4">
                         <v-icon color="#3283a6">mdi-calculator</v-icon>
                         </v-list-item-icon>
 
@@ -71,7 +71,7 @@
                         </v-list-item-content>
                     </v-list-item>
                     <v-list-item>
-                        <v-list-item-icon>
+                        <v-list-item-icon class="mr-4">
                         <v-icon color="#823289">mdi-page-layout-header-footer</v-icon>
                         </v-list-item-icon>
 
@@ -80,7 +80,7 @@
                         </v-list-item-content>
                     </v-list-item>
                     <v-list-item>
-                        <v-list-item-icon>
+                        <v-list-item-icon class="mr-4">
                         <v-icon color="#b81f78">mdi-tag-text-outline</v-icon>
                         </v-list-item-icon>
 
@@ -89,7 +89,7 @@
                         </v-list-item-content>
                     </v-list-item>
                     <v-list-item>
-                        <v-list-item-icon>
+                        <v-list-item-icon class="mr-4">
                         <v-icon color="#b4be26">mdi-chart-areaspline</v-icon>
                         </v-list-item-icon>
 
--- components/PostField.vue
+++ components/PostField.vue
@@ -1,38 +1,48 @@
 <template>
     <v-card class="pa-4" color="#142a46" dark outlined>
         <v-form>
-            <v-textarea
-            label="Hey Eazy, penny for your thoughts?"
-            prepend-inner-icon="mdi-account"
-            rows="3"
-            row-height="25"
-            v-model="postFieldModel"
-            color="primary"
-            :loading="postField__loading"
-            >{{ postFieldModel }}</v-textarea>
-            <div class="postField__image-container">
-                <v-btn small dark @click="postField__imageUploadBtn" icon>
-                    <v-icon color="primary">mdi-image-outline</v-icon>
-                </v-btn>
-                <v-btn rounded outlined small right absolute color="primary" @click="postField__submit">
-                    Post
-                </v-btn>
-            </div>
-            <div v-if="!postField__previewImage">
-                <input type="file" @change="onFileChange" class="d-none" ref="postField__inputRef">
-            </div>
-            <div v-else class="postField__preview pt-4">
-                <v-btn @click="removeImage" color="rgba(000,000,000,0.70)" fab x-small dark absolute>
-                    <v-icon color="white">mdi-close</v-icon>
-                </v-btn>
-                <img :src="postField__previewImage" />
+            <v-avatar size="38" class="postField__avatar">
+                <img
+                alt="Avatar"
+                src="test.jpg"
+                >
+            </v-avatar>
+            <div class="postField__textareaContainer">
+                <v-textarea
+                label="Hey Eazy, penny for your thoughts?"
+                class="pt-0"
+                rows="3"
+                row-height="25"
+                v-model="postFieldModel"
+                color="primary"
+                required
+                :loading="postField__loading"
+                >{{ postFieldModel }}</v-textarea>
+                <div>
+                    <div v-if="!postField__previewImage">
+                        <input type="file" @change="onFileChange" class="d-none" ref="postField__inputRef" accept=".jpg, .jpeg, .png, .gif, .mp4, .webm">
+                    </div>
+                    <div v-else class="postField__preview pt-2">
+                        <v-btn @click="removeImage" color="rgba(000,000,000,0.70)" fab x-small dark absolute>
+                            <v-icon color="white">mdi-close</v-icon>
+                        </v-btn>
+                        <img :src="postField__previewImage" class="postField__previewImage" v-if="post__isImage"/>
+                        <video controls :src="postField__previewImage" class="postField__previewImage" v-if="!post__isImage"/>
+                    </div>
+                    <v-btn small dark @click="postField__imageUploadBtn" icon>
+                        <v-icon color="primary">mdi-image-outline</v-icon>
+                    </v-btn>
+                    <v-btn rounded outlined small right absolute color="primary" :disabled="!postFieldModel" @click="postField__submit">
+                        Post
+                    </v-btn>
+                </div>
             </div>
         </v-form>
         <v-snackbar
             v-model="postField__alert"
-            color="info"
+            color="success"
             >
-            {{ post__response }}
+            {{ post__responseMsg }}
             <v-btn
                 color="white"
                 text
@@ -44,15 +54,18 @@
     </v-card>
 </template>
 <style>
-.postField__image-btn {
-    display: flex;
-    transform: scale(0.575);
-    transform-origin: left;
-    margin-top: -25px;
-}
-.postField__preview img {
+.postField__preview img, .postField__preview video {
     max-width: 100%;
     max-height: 500px;
+}
+.postField__avatar {
+    position: absolute;
+}
+.postField__textareaContainer {
+    margin-left: 50px;
+}
+.postField__previewImage {
+    border-radius: 10px;
 }
 </style>
 <script>
@@ -61,7 +74,8 @@ export default {
     data() {
         return {
             postField__alert: false,
-            post__response: null,
+            post__responseMsg: null,
+            post__isImage: null,
             postFieldModel: null,
             postField__previewImage: null,
             postField__loading: false
@@ -77,13 +91,13 @@ export default {
                 status: 'active'
             })
             .then((response) => {
-                this.post__response = response.data.data.post.content;
+                this.post__responseMsg = response.data.message;
                 this.postField__alert = true;
                 this.postFieldModel = '';
                 this.postField__loading = false;
             })
             .catch(function (error) {
-                console.log(error);
+                console.log(error)
             });
         },
         postField__imageUploadBtn: function(e) {
@@ -94,15 +108,18 @@ export default {
             var files = e.target.files || e.dataTransfer.files;
             if (!files.length)
                 return;
+            var filetype = (files[0].type).split("/")[0];
+            if(filetype == 'image')
+                this.post__isImage = true;
+            else
+                this.post__isImage = false;
             this.createImage(files[0]);
         },
         createImage(file) {
             var postField__previewImage = new Image();
             var reader = new FileReader();
-            var vm = this;
-
             reader.onload = (e) => {
-                vm.postField__previewImage = e.target.result;
+                this.postField__previewImage = e.target.result;
             };
             reader.readAsDataURL(file);
         },
