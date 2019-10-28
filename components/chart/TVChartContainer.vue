ca135ba1fd5209dac79097013f1de9d7563a6408
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-10-28 22:51:19 +0800

    use vuejs tradinview implementation

--- components/chart/TVChartContainer.vue
+++ components/chart/TVChartContainer.vue
@@ -1,140 +1,122 @@
 <template>
-    <div class="TVChartContainer" :id="containerId" />
+<div class="TVChartContainer" :id="containerId" />
 </template>
 
 <script>
-//import Datafeed from "../services/charting_api/index";
-import Datafeed from "../../services/charting_api/index";
+
+function getLanguageFromURL() {
+  const regex = new RegExp('[\\?&]lang=([^&#]*)');
+  const results = regex.exec(window.location.search);
+  return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
 
 export default {
-  name: "TradingViewContainer",
-  head() {
-    return {
-      title: 'Tradingview in Nuxtjs Version',
-      script: [
-        {
-          src: "/vendor/charting_library/charting_library.min.js",
-          type: "text/javascript"
-        }
-      ]
-    };
-  },
-
-  data() {
-    return {
-      myWidget: null
-    };
-  },
-
+  name: 'TVChartContainer',
   props: {
-    containerId: {
-      default: "tv_chart_container",
-      type: String
+    symbol: {
+      default: 'AAPL',
+      type: String,
     },
     interval: {
-      default: "1m",
-      type: String
+      default: 'D',
+      type: String,
+    },
+    containerId: {
+      default: 'tv_chart_container',
+      type: String,
+    },
+    datafeedUrl: {
+      default: 'https://demo_feed.tradingview.com',
+      type: String,
     },
     libraryPath: {
-      default: "/vendor/charting_library/",
-      type: String
+      default: '/vendor/charting_library/',
+      type: String,
     },
     chartsStorageUrl: {
-      default: "https://saveload.tradingview.com",
-      type: String
+      default: 'https://saveload.tradingview.com',
+      type: String,
     },
     chartsStorageApiVersion: {
-      default: "1.1",
-      type: String
+      default: '1.1',
+      type: String,
     },
     clientId: {
-      default: "tradingview.com",
-      type: String
+      default: 'tradingview.com',
+      type: String,
     },
     userId: {
-      default: "public_user_id",
-      type: String
+      default: 'public_user_id',
+      type: String,
     },
     fullscreen: {
       default: false,
-      type: Boolean
+      type: Boolean,
     },
     autosize: {
       default: true,
-      type: Boolean
+      type: Boolean,
     },
     studiesOverrides: {
-      type: Object
-    },
-    timezone: {
-      default: "Etc/UTC",
-      type: String
-    },
-  },
-
-  methods: {
-
-    initChartSetting() {
-      console.log("initChartSetting", this.currentTheme);
-      const widgetOptions = {
-        symbol: `Coinbase:USDT_BTC`,
-        datafeed: Datafeed,
-        interval: this.interval,
-        container_id: this.containerId,
-        library_path: this.libraryPath,
-
-        locale: "en",
-        charts_storage_url: this.chartsStorageUrl,
-        charts_storage_api_version: this.chartsStorageApiVersion,
-        client_id: this.clientId,
-        user_id: this.userId,
-        fullscreen: this.fullscreen,
-        autosize: this.autosize,
-      };
-
-      this.myWidget = new window.TradingView.widget(widgetOptions);
-
-       this.myWidget.onChartReady(
-        () => {
-          this.myWidget
-            .chart()
-            .onDataLoaded()
-            .subscribe(
-              null,
-              asd => {
-                console.log("onDataLoaded");
-                // listen socket here
-                // Datafeed.updateSocket();
-              },
-              error => {
-                console.log("onDataLoaded.error", error);
-              }
-            );
-        },
-        error => {
-          console.log("onChartReady.error", error);
-        }
-      );
+      type: Object,
     }
   },
-
+  tvWidget: null,
   mounted() {
-    // init chart setting
-    setTimeout(() => {
-      this.initChartSetting();
-    }, 500);
+    const widgetOptions = {
+      symbol: this.symbol,
+      // BEWARE: no trailing slash is expected in feed URL
+      datafeed: new window.Datafeeds.UDFCompatibleDatafeed(this.datafeedUrl),
+      interval: this.interval,
+      container_id: this.containerId,
+      library_path: this.libraryPath,
 
+      locale: getLanguageFromURL() || 'en',
+      disabled_features: ['use_localstorage_for_settings'],
+      enabled_features: ['study_templates'],
+      charts_storage_url: this.chartsStorageUrl,
+      charts_storage_api_version: this.chartsStorageApiVersion,
+      client_id: this.clientId,
+      user_id: this.userId,
+      fullscreen: this.fullscreen,
+      autosize: this.autosize,
+      studies_overrides: this.studiesOverrides,
+    };
 
-    // setTimeout(() => {
-    //   this.initChartSetting();
-    // }, 0);
+    const tvWidget = new window.TradingView.widget(widgetOptions);
+    this.tvWidget = tvWidget;
+
+    tvWidget.onChartReady(() => {
+      tvWidget.headerReady().then(() => {
+        const button = tvWidget.createButton();
+
+        button.setAttribute('title', 'Click to show a notification popup');
+        button.classList.add('apply-common-tooltip');
+
+        button.addEventListener('click', () => tvWidget.showNoticeDialog({
+            title: 'Notification',
+            body: 'TradingView Charting Library API works correctly',
+            callback: () => {
+              // eslint-disable-next-line no-console
+              console.log('Noticed!');
+            },
+          }));
+
+        button.innerHTML = 'Check API';
+      });
+    });
   },
-
-};
+  destroyed() {
+    if (this.tvWidget !== null) {
+      this.tvWidget.remove();
+      this.tvWidget = null;
+    }
+  }
+}
 </script>
 
-<style lang="css" scoped>
+<style scoped>
 .TVChartContainer {
-  height: 100vh;
+  height: calc(100vh - 80px);
 }
 </style>
--- pages/chart.vue
+++ pages/chart.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
         <h1>This is chart</h1>
-        <TVChartContainer />
+       <tv-chart-container></tv-chart-container>
     </div>
 </template>
 
 <script>
-import TVChartContainer from "../components/chart/TVChartContainer";
+// import TVChartContainer from "../components/chart/TVChartContainer";
 
 export default {
     layout: "main",
     components: {
-         TVChartContainer
+        //  TVChartContainer
     }
 }
 
