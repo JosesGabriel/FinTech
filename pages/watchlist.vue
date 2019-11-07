af11416e41fc0c662aa0a81227eab8996fba94d1
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-11-07 09:16:11 +0800

    Watchlists partial

--- pages/watchlist.vue
+++ pages/watchlist.vue
@@ -15,10 +15,23 @@
       </v-col>
       <v-col xs="12" sm="10" md="6" lg="6">
         <v-container class="pt-0" fluid>
-          <v-row dense>
-            <v-col v-for="n in 4" :key="n" cols="6" class="pt-0">
-              <WatchCard />
+          <div v-if="loadingBar" class="text-center">
+            <v-progress-circular
+              :size="50"
+              color="primary"
+              indeterminate
+            ></v-progress-circular>
+          </div>
+          <v-row>
+            <v-col
+              v-for="(n, index) in watchListObject.length"
+              :key="n"
+              cols="6"
+              class="pt-0"
+            >
+              <WatchCard :data="watchListObject[index]" />
             </v-col>
+            <AddWatcherModal v-if="!loadingBar" />
           </v-row>
         </v-container>
       </v-col>
@@ -41,6 +54,7 @@ import Navbar from "~/components/Navbar";
 import FooterSidebar from "~/components/FooterSidebar.vue";
 import TrendingStocks from "~/components/TrendingStocks.vue";
 import WatchCard from "~/components/watchers/WatchCard.vue";
+import AddWatcherModal from "~/components/watchers/AddWatcherModal.vue";
 
 export default {
   layout: "main",
@@ -48,13 +62,28 @@ export default {
     Navbar,
     TrendingStocks,
     FooterSidebar,
-    WatchCard
+    WatchCard,
+    AddWatcherModal
   },
   data() {
     return {
       isOpen: true,
-      isDarkMode: 0
+      isDarkMode: 0,
+      watchListObject: "",
+      loadingBar: true
     };
+  },
+  mounted() {
+    // GET Data from User Watchlist
+    let userData = {
+      user_id: "2d5486a1-8885-47bc-8ac6-d33b17ff7b58"
+    };
+    this.$axios
+      .$get("https://dev-api.arbitrage.ph/api/journal/watchlist", userData)
+      .then(response => {
+        this.watchListObject = response.data.watchlists;
+        this.loadingBar = false;
+      });
   },
   head() {
     return {
