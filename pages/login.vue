a638eaeee5b93852685636f6ed8fe16e59aa75c1
Joses Gabriel Lu <josesgabriellu@gmail.com>
2019-11-23 17:09:53 +0800

    auth

--- pages/login.vue
+++ pages/login.vue
@@ -2,10 +2,10 @@
   <v-card class="investagram_bulok text-center" color="transparent" dark>
     <span class="display-4 text-center d-inline-block">
       <marquee behavior="scroll" direction="left" scrollamount="50"
-        ><img
-          src="https://www.dairyqueen.com/Global/Food/Hot-Dogs_8-to-1_470x500.jpg?width=&height=810"
-          width="100px"
-      /></marquee>
+        ><iframe
+          src="https://www.youtube.com/embed/2g2bBK3ucRg?autoplay=1"
+        ></iframe
+      ></marquee>
       Bawal ang #MajorGaya
       <marquee behavior="scroll" direction="right" scrollamount="50"
         ><img
