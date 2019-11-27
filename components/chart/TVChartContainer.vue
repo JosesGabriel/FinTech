29c71bf333afdd579ab9950406f8df5e6dc687bc
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2019-11-27 15:13:24 +0800

    remove todo

--- components/chart/TVChartContainer.vue
+++ components/chart/TVChartContainer.vue
@@ -238,9 +238,6 @@ export default {
         .chart()
         .onSymbolChanged()
         .subscribe(null, function(symbolInfo) {
-          //TODO: ralph ito gamiton mo to query na for data sa sidebar ng chart
-          //TODO: take note na naka id_str to dapat para iwas javascript round-off
-          //TODO: @author: kbaluyot
           that.setSymbolID(symbolInfo.id_str);
           console.log(symbolInfo.id_str);
         });
