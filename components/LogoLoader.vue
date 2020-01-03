600f6247015b5e8d3524ddcf99ccb41fa75f054a
Emmanuel Tierra <emman.tierra@gmail.com>
2020-01-03 18:37:51 +0800

    Add LogoLoader global component

---
+++ components/LogoLoader.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-img
+        src="lyduz_loader.gif"
+        aspect-ratio="1"
+        contain="true"
+        max-width="500px"
+      ></v-img>
+    </v-row>
+  </v-container>
+</template>
