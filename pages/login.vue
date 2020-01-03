9e095cc5978d11763d7ef92561f40357f72209b7
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-01-03 15:41:20 +0800

    Test OG tags

--- pages/login.vue
+++ pages/login.vue
@@ -2,13 +2,13 @@
   <v-card class="investagram_bulok text-center" color="transparent" dark flat>
     <span class="display-4 text-center d-inline-block">
       <span class="font-weight-thin">
-        <img
+        <!-- <img
           class="pl-2 pt-1"
           src="lyduz_loader.gif"
           alt=""
           width="285"
           height="285"
-        />
+        /> -->
       </span>
     </span>
   </v-card>
