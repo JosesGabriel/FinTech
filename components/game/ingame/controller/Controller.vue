aa183672d4f610a8cc7a883b40e49dbe220ea728
Emmanuel Tierra <emman.tierra@gmail.com>
2020-01-06 18:16:52 +0800

    Refactor chart controller component

--- components/game/ingame/controller/Controller.vue
+++ components/game/ingame/controller/Controller.vue
@@ -1,17 +1,7 @@
 <template>
   <v-container class="pt-6">
     <v-row>
-      <v-col cols="12" class="text-center py-0">
-        <v-btn x-large dark icon>
-          <v-icon>mdi-stop</v-icon>
-        </v-btn>
-        <v-btn x-large dark icon>
-          <v-icon>mdi-play</v-icon>
-        </v-btn>
-        <v-btn x-large dark icon @click="nextButton()">
-          <v-icon>mdi-skip-next</v-icon>
-        </v-btn>
-      </v-col>
+      <ChartController />
     </v-row>
     <v-row>
       <BuyAndSell />
@@ -24,29 +14,12 @@
 }
 </style>
 <script>
-import { mapActions, mapGetters } from "vuex";
 import BuyAndSell from "./BuyAndSell";
+import ChartController from "./ChartController";
 export default {
   components: {
-    BuyAndSell
-  },
-  computed: {
-    ...mapGetters({
-      playerInGame: "game/getPlayerInGame",
-      lightSwitch: "global/getLightSwitch"
-    })
-  },
-  methods: {
-    ...mapActions({
-      setPlayerInGame: "game/setPlayerInGame"
-    }),
-    leaveGame() {
-      this.setPlayerInGame(false);
-    },
-    nextButton() {
-      var audio = new Audio("sounds/ping.wav");
-      audio.play();
-    }
+    BuyAndSell,
+    ChartController
   }
 };
 </script>
