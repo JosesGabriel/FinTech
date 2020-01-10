15bd42dd06ecf196ceeece420fdfec8f886d22ba
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-01-10 13:47:19 +0800

    set axios to client only and remove old mode keys

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,15 +54,15 @@ export default {
    ** Plugins to load before mounting the App
    */
   plugins: [
-    "~/plugins/axios",
-    { src: "~/plugins/bus", ssr: false },
-    { src: "~/plugins/repository", mode: "client", ssr: false },
-    { src: "~/plugins/global-method", ssr: true },
-    // { src: "~/plugins/numeral", ssr: false },
-    { src: "~/plugins/tradingview", mode: "client", ssr: false },
-    { src: "~/plugins/vue-numeral-filter.js", ssr: false },
+    "~/plugins/global-method",
+    { src: '~/plugins/axios', mode: "client" },
+    { src: "~/plugins/bus", mode: "client" },
+    { src: "~/plugins/repository", mode: "client"},
+    { src: "~/plugins/tradingview", mode: "client"},
+    { src: "~/plugins/vue-numeral-filter", mode: "client" },
     { src: "~/plugins/components/chart/vue-apexcharts", mode: "client" },
-    { src: "~/plugins/vue-native-websocket", mode: "client", ssr: false }
+    { src: "~/plugins/vue-native-websocket", mode: "client" }
+    // { src: "~/plugins/numeral", ssr: false },
   ],
   /*
    ** Nuxt.js dev-modules
@@ -76,6 +76,7 @@ export default {
    */
   modules: ["@nuxtjs/axios", "@nuxtjs/dotenv", "@nuxtjs/auth"],
   auth: {
+    watchLoggedIn: false,
     strategies: {
       local: {
         endpoints: {
