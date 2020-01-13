f54b0d65e1e55565129a27dd46386aed5ee9e14b
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-01-13 15:04:22 +0800

    add dynamic props assignment

--- pages/chart/_id.vue
+++ pages/chart/_id.vue
@@ -1,13 +1,25 @@
 <template>
   <v-content>
     <v-content>
-      <tv-chart-container></tv-chart-container>
+      <tv-chart-container
+        :dataFeed="dataFeed"
+        :chartsStorageUrl="'https://saveload.tradingview.com'"
+        :symbol="'PSE:PSEI'"
+        :clientId="'lyduz.com/chart'"
+      ></tv-chart-container>
     </v-content>
   </v-content>
 </template>
 
 <script>
+import Datafeed from "~/providers/tradingview/api";
+
 export default {
-  layout: "chart"
+  layout: "chart",
+  data(){
+    return {
+      dataFeed: Datafeed
+    }
+  }
 };
 </script>
