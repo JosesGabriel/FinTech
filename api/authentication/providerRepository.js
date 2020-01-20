53f682fb5ee9c310973063e59270fd9a6709a911
Emmanuel Tierra <emman.tierra@gmail.com>
2020-01-20 13:06:39 +0800

    Add provider redirect callback request

--- api/authentication/providerRepository.js
+++ api/authentication/providerRepository.js
@@ -3,5 +3,8 @@ const baseURL = process.env.API_URL + "/auth";
 export default $axios => ({
   getLoginUrl(provider) {
     return $axios.get(`${baseURL}/login/${provider}`);
+  },
+  getRedirectCallback(provider) {
+    return $axios.get(`${baseURL}/login/${provider}/callback`);
   }
 });
