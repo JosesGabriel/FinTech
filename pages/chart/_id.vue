59e63439d3c28257f56655586bd6388e5d800d3d
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-01-24 01:06:58 +0800

    test

--- pages/chart/_id.vue
+++ pages/chart/_id.vue
@@ -1,12 +1,12 @@
 <template>
   <v-content>
     <v-content>
-      <tv-chart-container
+      <!-- <tv-chart-container
         :dataFeed="dataFeed"
         :chartsStorageUrl="'https://saveload.tradingview.com'"
         :symbol="'PSE:PSEI'"
         :clientId="'lyduz.com/chart'"
-      ></tv-chart-container>
+      ></tv-chart-container> -->
     </v-content>
   </v-content>
 </template>
