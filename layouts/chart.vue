a372d17ea97501d4390fe1696e7ac6623246ceb3
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-01-24 01:17:52 +0800

    for testing

--- layouts/chart.vue
+++ layouts/chart.vue
@@ -5,7 +5,7 @@
     class="gameGlobal"
     :style="{ background: cardBackground }"
   >
-    <Header />
+    <!-- <Header /> -->
     <div class="spacer__content"></div>
     <div class="content__main">
       <div class="content__left">
@@ -34,7 +34,7 @@
           <v-col class="col-12">
             <v-slide-y-reverse-transition>
               <v-content class="table__container">
-                <Table />
+                <!-- <Table /> -->
               </v-content>
             </v-slide-y-reverse-transition>
           </v-col>
@@ -51,7 +51,7 @@
           { right_light_mode: lightSwitch == 0 }
         ]"
       >
-        <Sidebar />
+        <!-- <Sidebar /> -->
       </div>
     </div>
   </v-app>
