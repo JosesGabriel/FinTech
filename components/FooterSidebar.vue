6b163f85562d6f1f32cc1cfff3ce10acf890ac80
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-02-03 08:13:13 +0800

    Removed underlines on footer links

--- components/FooterSidebar.vue
+++ components/FooterSidebar.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="footer-main">
-    <router-link to="/privacy-policy">
-        <span class="caption tertiary--text">Privacy Policy •</span>
+    <router-link class="no-transform" to="/privacy-policy">
+      <span class="caption tertiary--text">Privacy Policy •</span>
     </router-link>
-    <router-link to="/terms-of-use">
-        <span class="caption tertiary--text">Terms of Use •</span>
+    <router-link class="no-transform" to="/terms-of-use">
+      <span class="caption tertiary--text">Terms of Use •</span>
     </router-link>
-    <router-link to="/contact-us">
-        <span class="caption tertiary--text">Contact Us •</span>
+    <router-link class="no-transform" to="/contact-us">
+      <span class="caption tertiary--text">Contact Us •</span>
     </router-link>
-    <router-link to="/about-us">
-    <span class="caption tertiary--text">About Us •</span>
-    
+    <router-link class="no-transform" to="/about-us">
+      <span class="caption tertiary--text">About Us •</span>
     </router-link>
     <a class="caption success--text">Lyduz © 2020</a>
     <v-icon small color="tertiary">mdi-facebook</v-icon>
