02ab839d1bb64816fd18e46176c660fba216f189
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-02-19 16:07:57 +0800

    lower symbol request delay

--- components/chart/TVChartContainer.vue
+++ components/chart/TVChartContainer.vue
@@ -255,7 +255,7 @@ export default {
         enabled_features: this.enabledFeatures,
         toolbar_bg: this.lightSwitch == 0 ? "#F2F2F2" : "#00121e", // FIXME configs for hex color
         time_frames: this.timeFrames,
-        symbol_search_request_delay: 300, // strict delay to avoid API request overload
+        symbol_search_request_delay: 10, // strict delay to avoid API request overload
         debug: false,
         datafeed: this.dataFeed,
         locale: this.getLanguageFromURL() || "en",
