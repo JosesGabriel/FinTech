9dc6c76fa3b2c629a594a2787589b7d20fc15cb6
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-03-03 18:07:33 +0800

    Cancer code for ads (remove after march 4)

--- components/social/Ad.vue
+++ components/social/Ad.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-card class="mx-auto ad__card" :dark="lightSwitch == 0 ? false : true">
+  <v-card
+    class="mx-auto ad__card"
+    :ripple="false"
+    :dark="lightSwitch == 0 ? false : true"
+    @click="redirect()"
+  >
     <v-list-item>
       <v-list-item-content>
         <v-list-item-title class="body-2 font-weight-black"
@@ -20,6 +25,11 @@ export default {
       userWatchedStocks: "watchers/getUserWatchedStocks",
       lightSwitch: "global/getLightSwitch"
     })
+  },
+  methods: {
+    redirect() {
+      window.open("http://bit.ly/MyTradeAd", "_blank");
+    }
   }
 };
 </script>
@@ -27,5 +37,6 @@ export default {
 <style>
 .ad__card {
   border: thin solid rgba(255, 255, 255, 0.12) !important;
+  cursor: pointer;
 }
 </style>
