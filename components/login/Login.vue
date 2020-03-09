5f192211027fe11a658766797bb3773fb8a6e454
Ralph Tolipas <tolipasralphryan10@gmail.com>
2020-03-09 16:09:00 +0800

    test refresh token

--- components/login/Login.vue
+++ components/login/Login.vue
@@ -82,7 +82,7 @@
 
       <v-hover v-slot:default="{ hover }">
         <v-btn
-          v-show="false"
+          v-show="true"
           block
           rounded
           class="black--text font-weight-bold text-capitalize mb-2"
@@ -165,7 +165,7 @@ export default {
 
         // reload for proper component mounting
         setTimeout(() => {
-          window.open("/", "_self");
+          // window.open("/", "_self");
         }, 800);
       } catch (error) {
         this.$emit("alert", {
