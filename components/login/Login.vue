d128777697795f3baffd30c8b329b143e933a9e4
Ralph Tolipas <tolipasralphryan10@gmail.com>
2020-03-09 14:44:45 +0800

    test refresh token

--- components/login/Login.vue
+++ components/login/Login.vue
@@ -90,23 +90,10 @@
           elevation="1"
           @click.prevent="refresh()"
         >
-          Refresh Arbi
+          Refresh
         </v-btn>
       </v-hover>
 
-      <v-hover v-slot:default="{ hover }">
-        <v-btn
-          v-show="true"
-          block
-          rounded
-          class="black--text font-weight-bold text-capitalize mb-2"
-          :color="!hover ? 'success' : 'successhover'"
-          elevation="1"
-          @click.prevent="refresh2()"
-        >
-          Refresh Lyduz
-        </v-btn>
-      </v-hover>
       <span class="text-center d-block caption w-100"
         >New to Lyduz?
         <a
@@ -145,38 +132,12 @@ export default {
   methods: {
     async refresh() {
       try {
-        console.log(this.$cookies.getAll({ fromRes: true }));
-        console.log(this.$cookies.getAll({ fromRes: true }));
-
         const response = await this.$axios.$post(
-          "https://dev-api.arbitrage.ph/api/auth/login/refresh",
+          `${process.env.API_URL}/auth/login/refresh`,
           {},
           { credentials: true }
         );
         console.log("response", response);
-
-        const response2 = await this.$axios.$post(
-          "https://dev-api.lyduz.com/api/auth/login/refresh",
-          {},
-          { credentials: true }
-        );
-        console.log("response2", response2);
-      } catch (error) {
-        console.log(error);
-      }
-    },
-
-    async refresh2() {
-      try {
-        console.log(this.$cookies.getAll({ fromRes: true }));
-        console.log(this.$cookies.getAll({ fromRes: true }));
-
-        const response2 = await this.$axios.$post(
-          "https://dev-api.lyduz.com/api/auth/login/refresh",
-          {},
-          { credentials: true }
-        );
-        console.log("response2", response2);
       } catch (error) {
         console.log(error);
       }
@@ -204,7 +165,7 @@ export default {
 
         // reload for proper component mounting
         setTimeout(() => {
-          //window.open("/", "_self");
+          window.open("/", "_self");
         }, 800);
       } catch (error) {
         this.$emit("alert", {
