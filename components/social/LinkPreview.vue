2b098a80c282e2ddaaa6aa39f424ee20193ea42b
Ralph Tolipas <tolipasralphryan10@gmail.com>
2020-03-13 13:50:15 +0800

    update link preview design aligned on figma

--- components/social/LinkPreview.vue
+++ components/social/LinkPreview.vue
@@ -1,19 +1,38 @@
 <template>
   <v-container class="pa-0">
-    <v-row v-show="hasImage">
+    <v-row v-if="hasImage">
       <v-col cols="12" class="pa-0 pr-1">
         <div class="newsfeed__imageWrapper">
           <v-hover>
             <template v-slot:default="{ hover }">
               <v-img
                 class="newsfeed__image"
-                position="top"
-                :alt="link.title"
-                :src="link.image"
-                :title="link.title"
+                :src="image"
                 lazy-src="/blur.jpg"
-                min-height="250"
+                min-height="250px"
               >
+                <v-content
+                  v-show="hasDescription"
+                  class="description__container"
+                >
+                  <v-card class="mx-auto" min-height="120px" tile outlined>
+                    <v-list-item three-line>
+                      <v-list-item-content>
+                        <v-list-item-title class="title mb-1">{{
+                          title
+                        }}</v-list-item-title>
+                        <v-list-item-subtitle
+                          >{{ description }}
+                        </v-list-item-subtitle>
+
+                        <v-list-item-action-text class="mt-6">
+                          <div class="overline">{{ url }}</div>
+                        </v-list-item-action-text>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-card>
+                </v-content>
+                <!-- template animation -->
                 <template v-slot:placeholder>
                   <v-row
                     class="fill-height ma-0"
@@ -22,7 +41,8 @@
                   >
                     <v-progress-circular
                       indeterminate
-                      color="grey lighten-5"
+                      size="64"
+                      color="#03dac5"
                     ></v-progress-circular>
                   </v-row>
                 </template>
@@ -31,9 +51,10 @@
                     <v-hover v-slot:default="{ onHover }">
                       <v-btn
                         class="black--text font-weight-bold text-capitalize body-1"
-                        :color="!onHover ? 'success' : 'successhover'"
-                        outlined
+                        :outlined="!onHover"
+                        color="success"
                         x-large
+                        elevation="1"
                         @click.prevent="visitLink()"
                         >Visit Link</v-btn
                       >
@@ -48,7 +69,62 @@
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  props: {
+    meta: {
+      default: null,
+      type: Object
+    }
+  },
+  computed: {
+    hasImage() {
+      if (this.meta != null && this.meta.links[0].meta != undefined) {
+        return this.meta.links[0].meta.image != undefined ? true : false;
+      }
+      return false;
+    },
+    hasDescription() {
+      if (this.meta != null && this.meta.links[0].meta != undefined) {
+        return this.meta.links[0].meta.description != undefined ? true : false;
+      }
+      return false;
+    },
+    title() {
+      if (this.meta.links[0].meta.title != undefined) {
+        return this.meta.links[0].meta.title;
+      }
+      return "";
+    },
+    description() {
+      return this.meta.links[0].meta.description;
+    },
+    image() {
+      return this.meta.links[0].meta.image;
+    },
+    url() {
+      return this.meta.links[0].url;
+    }
+  },
+  methods: {
+    visitLink() {
+      this.$emit("visitLink", this.meta.links[0].url);
+    }
+  }
+};
+</script>
+
 <style scoped>
+.description__container {
+  position: absolute;
+  background: #ffffff;
+  opacity: 0.7;
+  min-height: 120px;
+  bottom: 0;
+  width: 100%;
+  margin-left: -1px;
+}
 .lowOpacity {
   opacity: 0.2;
 }
@@ -61,7 +137,7 @@
   width: 100%;
 }
 .newsfeed__image {
-  width: 101%;
+  width: 100%;
   padding: 13px;
   height: auto;
 }
@@ -69,31 +145,3 @@
   max-height: 500px;
 }
 </style>
-<script>
-export default {
-  props: {
-    meta: {
-      default: null,
-      type: Object
-    }
-  },
-  data() {
-    return {
-      dialog: false
-    };
-  },
-  computed: {
-    hasImage() {
-      return this.meta != null && this.meta.links[0].meta.image ? true : false;
-    },
-    link() {
-      return this.meta.links[0].meta;
-    }
-  },
-  methods: {
-    visitLink() {
-      this.$emit("visitLink", this.meta.links[0].url);
-    }
-  }
-};
-</script>
