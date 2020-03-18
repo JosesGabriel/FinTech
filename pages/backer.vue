c59c7bfeb54406ae9347964be3c7cd7350542e4b
Rocky Gonzales <rocky@arbitrage.ph>
2020-03-18 09:28:56 +0800

    update trade modal

--- pages/backer.vue
+++ pages/backer.vue
@@ -30,7 +30,9 @@
           <span class="backer__subheadline--text d-block">
             Building the best community of investors from all over
             the world housed under an
-            <strong class="success--text">end to end platform</strong> where
+            <strong
+              class="success--text"
+            >end to end platform</strong> where
             idea sharing is promoted, mutual respect is upheld
             and above all, technology cuts above the rest.
           </span>
@@ -40,10 +42,14 @@
           <span class="backer__quoteheadline--text d-block text-justify">
             We aim to create the LinkedIn for retail traders and self-directed
             investors, packed with
-            <strong class="success--text">institutional-grade</strong> features, insights, market
+            <strong
+              class="success--text"
+            >institutional-grade</strong> features, insights, market
             intelligence and everything that is in-between, all streamed into a
             data superhighway that enables any user to
-            <strong class="success--text">early detect</strong> real-world
+            <strong
+              class="success--text"
+            >early detect</strong> real-world
             market opportunities.
           </span>
           <v-divider :dark="lightSwitch == 1" class="mt-10" width="85%"></v-divider>
@@ -88,9 +94,25 @@
               Note: Contents provided by third parties are excluded.
               <br />Offer is limited to the first 6,000 supporters only.
             </span>
-            <v-btn rounded color="success" block class="black--text text-capitalize bold mt-8">
-              Back
-              <span class="text-lowercase mx-1">us</span> Today
+            <v-btn
+              @click="checkUserLogin"
+              rounded
+              color="success"
+              block
+              class="black--text text-capitalize bold mt-8"
+            >
+              <span v-show="!toggleElements">
+                Back
+                <span class="text-lowercase mx-1">us</span> Today
+              </span>
+              <v-progress-circular
+                v-show="toggleElements"
+                small
+                indeterminate
+                color="gray"
+                :width="2"
+                :size="20"
+              ></v-progress-circular>
             </v-btn>
           </div>
         </v-card>
@@ -112,6 +134,7 @@ export default {
   },
   data() {
     return {
+      toggleElements: false,
       items: [
         {
           src: "/background-carousel/bg-id1.jpg"
@@ -121,6 +144,28 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    checkUserLogin() {
+      this.toggleElements = true;
+      switch (this.$auth.state.loggedIn) {
+        case true:
+          this.$api.accounts.payment.payments().then(response => {
+            if (response.success) {
+              window.location.href = response.meta.redirect.url;
+
+              this.toggleElements = false;
+            }
+          });
+          break;
+        case false:
+          window.location.href = "/login";
+          break;
+
+        default:
+          break;
+      }
+    }
   }
 };
 </script>
