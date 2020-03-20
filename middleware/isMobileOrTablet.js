f988ee44fca1a516a5bfe514f560a21405fd2c85
nick <nickgayo12@gmail.com>
2020-03-20 13:39:07 +0800

    remove temporary pages

--- middleware/isMobileOrTablet.js
+++ middleware/isMobileOrTablet.js
@@ -2,8 +2,9 @@ export default function(context) {
     if (context.isMobileOrTablet === true) {
         // add here pages that will not block from mobile users
         // set page limit for mobile users
-        const allowedPages = ["login", "verify", ""];
-        if (allowedPages.includes(context.route.name) === false) {
+        const allowedPages = [];
+        // const allowedPages = ["login", "verify", ""];
+        if (allowedPages.includes(context.route.name) === true) {
             return context.redirect("/mobile");
         }
     }
