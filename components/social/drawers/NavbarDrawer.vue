8c98115668e57304a523645e28b9984916c671ab
Joses Gabriel Lu <josesgabriellu@gmail.com>
2020-03-23 10:45:58 +0800

    Bugfix on navbardrawer element

--- components/social/drawers/NavbarDrawer.vue
+++ components/social/drawers/NavbarDrawer.vue
@@ -1,114 +1,158 @@
 <template>
-<v-card class="navbar__card" flat color="transparent"></v-card>
-  <v-navigation-drawer floating :dark="lightSwitch == 1" class="mt-10 pt-3" v-model="drawer" absolute temporary>
-    <v-list height="100%" nav dense rounded class="py-0 pt-5">
-      <v-list-item-group>
-        <router-link
-          :to="
+  <v-card class="navbar__card" flat color="transparent">
+    <v-navigation-drawer
+      v-model="drawer"
+      floating
+      :dark="lightSwitch == 1"
+      class="mt-10 pt-3"
+      absolute
+      temporary
+    >
+      <v-list height="100%" nav dense rounded class="py-0 pt-5">
+        <v-list-item-group>
+          <router-link
+            :to="
               $auth.loggedIn ? '/profile/' + $auth.user.data.user.username : ''
             "
-          class="no-transform"
-        >
-          <v-list-item class="tertiary--text">
-            <v-list-item-avatar :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4' ]" size="24">
-              <v-img
-                class="avatar__border"
-                :src="
+            class="no-transform"
+          >
+            <v-list-item class="tertiary--text">
+              <v-list-item-avatar
+                :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4']"
+                size="24"
+              >
+                <v-img
+                  class="avatar__border"
+                  :src="
                     $auth.loggedIn
                       ? $auth.user.data.user.profile_image
                         ? $auth.user.data.user.profile_image
                         : 'default.png'
                       : ''
                   "
-              ></v-img>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title class="navbar__name body-2">
-                {{
-                $auth.loggedIn ? $auth.user.data.user.name : "Guest"
-                }}
-              </v-list-item-title>
-              <span class="overline no-transform">@{{ $auth.user.data.user.username }}</span>
-            </v-list-item-content>
-          </v-list-item>
-          <div v-if="!data">
-            <v-divider></v-divider>
-            <v-divider class="mb-1"></v-divider>
-          </div>
-        </router-link>
-        <router-link to="/" class="no-transform">
-          <v-list-item
-            :class="active == 'social' ? 'v-list-item--active' : ''"
-            class="tertiary--text"
-          >
-            <v-list-item-icon :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4' ]">
-              <v-icon :color="active == 'social' ? 'success' : ''">mdi-account-supervisor-circle</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title
-                class="font-weight-black no-transform body-2"
-                :class="lightSwitch == 0 ? 'black--text' : 'white--text'"
-              >Social Wall</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link>
-        <router-link to="/chart" class="no-transform">
-          <v-list-item
-            :class="active == 'chart' ? 'v-list-item--active' : ''"
-            class="tertiary--text"
-          >
-            <v-list-item-icon :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4' ]">
-              <v-icon :color="active == 'chart' ? 'success' : ''">mdi-chart-bar</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title class="font-weight-medium no-transform body-2">Interactive Chart</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link>
+                ></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="navbar__name body-2">
+                  {{ $auth.loggedIn ? $auth.user.data.user.name : "Guest" }}
+                </v-list-item-title>
+                <span class="overline no-transform"
+                  >@{{ $auth.user.data.user.username }}</span
+                >
+              </v-list-item-content>
+            </v-list-item>
+            <div v-if="!data">
+              <v-divider></v-divider>
+              <v-divider class="mb-1"></v-divider>
+            </div>
+          </router-link>
+          <router-link to="/" class="no-transform">
+            <v-list-item
+              :class="active == 'social' ? 'v-list-item--active' : ''"
+              class="tertiary--text"
+            >
+              <v-list-item-icon
+                :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4']"
+              >
+                <v-icon :color="active == 'social' ? 'success' : ''"
+                  >mdi-account-supervisor-circle</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  class="font-weight-black no-transform body-2"
+                  :class="lightSwitch == 0 ? 'black--text' : 'white--text'"
+                  >Social Wall</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
+          <router-link to="/chart" class="no-transform">
+            <v-list-item
+              :class="active == 'chart' ? 'v-list-item--active' : ''"
+              class="tertiary--text"
+            >
+              <v-list-item-icon
+                :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4']"
+              >
+                <v-icon :color="active == 'chart' ? 'success' : ''"
+                  >mdi-chart-bar</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  class="font-weight-medium no-transform body-2"
+                  >Interactive Chart</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
 
-        <router-link to="/journal" class="no-transform">
-          <v-list-item
-            :class="active == 'journal' ? 'v-list-item--active' : ''"
-            class="tertiary--text"
-          >
-            <v-list-item-icon :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4' ]">
-              <v-icon :color="active == 'journal' ? 'success' : ''">mdi-text-box-multiple</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title class="font-weight-medium no-transform body-2">Trading Journal</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link>
+          <router-link to="/journal" class="no-transform">
+            <v-list-item
+              :class="active == 'journal' ? 'v-list-item--active' : ''"
+              class="tertiary--text"
+            >
+              <v-list-item-icon
+                :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4']"
+              >
+                <v-icon :color="active == 'journal' ? 'success' : ''"
+                  >mdi-text-box-multiple</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  class="font-weight-medium no-transform body-2"
+                  >Trading Journal</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
 
-        <router-link to="/trade-simulator" class="no-transform">
-          <v-list-item
-            :class="active == 'virtualtrade' ? 'v-list-item--active' : ''"
-            class="tertiary--text"
-          >
-            <v-list-item-icon :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4' ]">
-              <v-icon :color="active == 'virtualtrade' ? 'success' : ''">mdi-cloud-print-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title class="font-weight-medium no-transform body-2">Virtual Trading</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link>
-        <router-link to="/watchlist" class="no-transform">
-          <v-list-item
-            :class="active == 'watchlist' ? 'v-list-item--active' : ''"
-            class="tertiary--text"
-          >
-            <v-list-item-icon :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4' ]">
-              <v-icon :color="active == 'watchlist' ? 'success' : ''">mdi-eye-plus-outline</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title class="font-weight-medium no-transform body-2">Watchlist</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link>
-      </v-list-item-group>
-    </v-list>
-  </v-navigation-drawer>
+          <router-link to="/trade-simulator" class="no-transform">
+            <v-list-item
+              :class="active == 'virtualtrade' ? 'v-list-item--active' : ''"
+              class="tertiary--text"
+            >
+              <v-list-item-icon
+                :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4']"
+              >
+                <v-icon :color="active == 'virtualtrade' ? 'success' : ''"
+                  >mdi-cloud-print-outline</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  class="font-weight-medium no-transform body-2"
+                  >Virtual Trading</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
+          <router-link to="/watchlist" class="no-transform">
+            <v-list-item
+              :class="active == 'watchlist' ? 'v-list-item--active' : ''"
+              class="tertiary--text"
+            >
+              <v-list-item-icon
+                :class="[$vuetify.breakpoint.mdAndDown ? '' : 'mr-4']"
+              >
+                <v-icon :color="active == 'watchlist' ? 'success' : ''"
+                  >mdi-eye-plus-outline</v-icon
+                >
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title
+                  class="font-weight-medium no-transform body-2"
+                  >Watchlist</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </router-link>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer></v-card
+  >
 </template>
 
 <script>
