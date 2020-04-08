35bb16ee6796585a9fb837bd89e5bf6bf8372186
nick <nickgayo12@gmail.com>
2020-04-08 13:08:50 +0800

    add process notice, redirect to login if user not login

--- pages/sso.vue
+++ pages/sso.vue
@@ -1,10 +1,18 @@
-<template></template>
+<template>
+  <v-container>
+  </v-container>
+</template>
 <script>
 export default {
   auth: false,
   layout: "main",
   mounted() {
-    
+    this.checkUserAuth()    
+  },
+  methods: {
+    checkUserAuth() {
+
+    }
   }
 };
 </script>
